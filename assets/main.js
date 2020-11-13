98350f1bb4d95e8d9dada349777062d1a6e56507
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-13 15:22:15 +0100

    Finish
    added some style to the index file.

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
 //**Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 var chilometri = Number(prompt("Quanti km devi percorrere?"));
 var etàPasseggero = Number(prompt("Quanti anni hai?"));
-
+document.getElementById('chilometri').innerHTML = "Km : " + chilometri;
+document.getElementById('etàPasseggero').innerHTML = "Età : " + etàPasseggero;
 console.log(chilometri, etàPasseggero);
 
 //**Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
@@ -9,8 +10,7 @@ console.log(chilometri, etàPasseggero);
 
 var prezzoChilometri = 0.21;
 var prezzoBiglietto = chilometri * prezzoChilometri;
-
-console.log(prezzoBiglietto);
+//console.log(prezzoBiglietto);
 
 //**va applicato uno sconto del 20% per i minorenni
 var discountYoung = .20;
@@ -19,11 +19,21 @@ var newTicket
 
 if (etàPasseggero < 18){
     newTicket = prezzoBiglietto  - (prezzoBiglietto * 0.2);
-    console.log(newTicket);
+    //console.log(newTicket);
+    document.getElementById('discount').innerHTML = "Prezzo: " + newTicket.toFixed(2) + " euro";
 }
+
+//**non va applicato nessuno sconto se età compresa tra 18 & 65
+else if (etàPasseggero > 18 && etàPasseggero != 66){
+    console.log(prezzoBiglietto)
+    document.getElementById('prezzo-biglietto').innerHTML ="Prezzo: " + prezzoBiglietto.toFixed(2) + " euro";
+}
+
+
 //**va applicato uno sconto del 40% per gli over 65
 
 else if (etàPasseggero > 65){
     newTicket = prezzoBiglietto  - (prezzoBiglietto * 0.4);
-    console.log(newTicket)
+    //console.log(newTicket)
+    document.getElementById('discount').innerHTML = "Prezzo: " + newTicket.toFixed(2) + " euro";
 }
